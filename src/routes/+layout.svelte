01555169b5911e140949510e6cd542b757c254bf
Ivan <ivangyr321@gmail.com>
2024-12-14 01:23:53 -0500

    Actualiza el diseño de +layout.svelte: mejora la navegación, ajusta estilos y agrega bordes a los elementos principales.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
 	import '../app.css';
-	let { children } = $props();
 	import ThemeToggle from '$lib/components/ThemeToggle.svelte';
+
+	let { children } = $props();
 </script>
 
-<div class="min-h-screen bg-gray-200 dark:bg-gray-900">
+<div class="min-h-screen bg-zinc-300 dark:bg-zinc-950">
 	<!-- Header con navegación -->
-	<header>
-		<nav class="container mx-auto px-4 py-4" aria-label="navegación principal">
+	<header class="border-b-2 border-black dark:border-white">
+		<nav class="container mx-auto px-4 py-6" aria-label="navegación principal">
 			<div class="flex items-center justify-between">
-				<a href="/" class="text-xl font-bold text-gray-800 dark:text-white"> Ivan </a>
+				<a
+					href="/"
+					class="font-cabinet border-2 border-black px-4
+                           py-2 text-xl font-bold text-black
+						    
+						   hover:-translate-y-0.5
+						   hover:translate-x-0.5
+						   active:translate-x-0
+						   active:translate-y-0 dark:border-white
+                           dark:text-white"
+				>
+					Ivan
+				</a>
 				<ThemeToggle />
 			</div>
 		</nav>
@@ -17,11 +30,16 @@
 
 	<!-- Contenido principal -->
 	<main class="container mx-auto px-4 py-8">
-		{@render children()}
+		<div class="border-2 border-black p-8 dark:border-white">
+			{@render children()}
+		</div>
 	</main>
 
 	<!-- Footer -->
-	<footer class="container mx-auto px-4 py-6 text-gray-600 dark:text-gray-400">
-		<p class="text-center">© {new Date().getFullYear()} - Creado con 💖</p>
+	<footer class="container mx-auto border-t-2 border-black px-4 py-6 dark:border-white">
+		<p class=" text-center text-zinc-700 dark:text-zinc-300">
+			© {new Date().getFullYear()} - Creado con
+			<span class="inline-block hover:-translate-y-1">💖</span>
+		</p>
 	</footer>
 </div>
