d43d121da3cabe11041c40dce19a637b4c401834
Ivan <ivangyr321@gmail.com>
2024-12-14 17:56:42 -0500

    Refactor +page.svelte: simplifica la estructura del componente eliminando un contenedor innecesario

--- src/routes/practicando/+page.svelte
+++ src/routes/practicando/+page.svelte
@@ -1,16 +1,14 @@
-<div class="space-y-8">
-	<h1 class="heading-1">Practicando</h1>
+<h1 class="heading-1">Practicando</h1>
 
-	<div
-		class="rounded-3xl border-2 border-sky-900 p-6 dark:border-sky-100
+<div
+	class="rounded-3xl border-2 border-sky-900 p-6 dark:border-sky-100
                  "
-	>
-		<h2 class="heading-2">Título 2</h2>
+>
+	<h2 class="heading-2">Título 2</h2>
 
-		<p class="p">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quisquam nihil animi,
-			consequatur qui perferendis placeat ipsum dolores sequi quas rem mollitia debitis officia
-			architecto autem quo repellendus ducimus maiores?
-		</p>
-	</div>
+	<p class="p">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quisquam nihil animi, consequatur
+		qui perferendis placeat ipsum dolores sequi quas rem mollitia debitis officia architecto autem
+		quo repellendus ducimus maiores?
+	</p>
 </div>
