ba7c83214a0e140c014b8a8ed676d31c3832d48c
Ivan <ivangyr321@gmail.com>
2024-12-14 02:26:53 -0500

    Agrega componentes Footer.svelte y Header.svelte: implementa el pie de página y la navegación principal con soporte para el cambio de tema.

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,8 @@
+<div class="border-t-2 border-black dark:border-white">
+	<footer class="container mx-auto px-4 py-6" aria-label="pie de página">
+		<p class="text-center text-zinc-700 dark:text-zinc-300">
+			© {new Date().getFullYear()} - Creado con
+			<span class="inline-block hover:-translate-y-1">💖</span>
+		</p>
+	</footer>
+</div>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import ThemeToggle from './ThemeToggle.svelte';
+</script>
+
+<header class="border-b-2 border-black dark:border-white">
+	<nav class="container mx-auto px-4 py-6" aria-label="navegación principal">
+		<div class="flex items-center justify-between">
+			<a
+				href="/"
+				class="font-cabinet border-2 border-black px-4
+                       py-2 text-xl font-bold text-black
+                       hover:-translate-y-0.5 hover:translate-x-0.5
+                       active:translate-x-0 active:translate-y-0
+                       dark:border-white dark:text-white"
+			>
+				Ivan
+			</a>
+			<ThemeToggle />
+		</div>
+	</nav>
+</header>
