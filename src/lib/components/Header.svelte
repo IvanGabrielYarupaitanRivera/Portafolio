9d7c74695eba4bf6c40e2ca9f9c7fae74a143d73
Ivan <ivangyr321@gmail.com>
2024-12-14 15:58:20 -0500

    Refactor Header.svelte: actualiza el color del borde inferior en modo oscuro a sky-100

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,7 +2,7 @@
 	import NavMenu from './NavMenu.svelte';
 </script>
 
-<header class="rounded-3xl border-b-2 border-sky-900 dark:border-sky-300">
+<header class="rounded-3xl border-b-2 border-sky-900 dark:border-sky-100">
 	<nav class="container mx-auto px-4 py-6" aria-label="navegación principal">
 		<div class="flex justify-between">
 			<a
@@ -10,7 +10,7 @@
 				class="font-cabinet rounded-3xl border-2 border-sky-900 bg-orange-400 px-4 py-2
 					   text-xl font-bold text-slate-800 hover:-translate-y-0.5
 					   hover:translate-x-0.5 hover:bg-orange-300 active:translate-x-0 active:translate-y-0
-					   dark:border-sky-300 dark:bg-orange-600 dark:text-slate-50
+					   dark:border-sky-100 dark:bg-orange-600 dark:text-slate-50
 					    dark:hover:bg-orange-700"
 			>
 				Ivan Gabriel
