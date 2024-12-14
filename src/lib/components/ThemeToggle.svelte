4a1a5234f5cd5a993aa6385286ceecb3a338236a
Ivan <ivangyr321@gmail.com>
2024-12-14 09:53:14 -0500

    Refactor ThemeToggle.svelte: simplifica estilos del botón de cambio de tema y mejora la transición de colores

--- src/lib/components/ThemeToggle.svelte
+++ src/lib/components/ThemeToggle.svelte
@@ -8,20 +8,13 @@
 
 <button
 	on:click={toggleTheme}
-	class="relative rounded-3xl border-2 border-sky-600 p-2 hover:-translate-y-0.5
-             
-           hover:translate-x-0.5 hover:border-orange-600
-
-            hover:bg-orange-600/10
-           active:translate-x-0 active:translate-y-0
-           dark:border-sky-400
-           dark:hover:border-orange-500 dark:hover:bg-orange-500/10"
+	class="relative rounded-3xl border-2 border-sky-600 p-2 hover:-translate-y-0.5 hover:translate-x-0.5 active:translate-x-0 active:translate-y-0 dark:border-sky-400"
 	aria-label="Cambiar tema"
 >
 	<SunMoon
 		size={24}
 		strokeWidth={2}
-		class="stroke-slate-800 transition-colors duration-300
+		class="stroke-slate-800  
                dark:stroke-slate-50"
 	/>
 </button>
