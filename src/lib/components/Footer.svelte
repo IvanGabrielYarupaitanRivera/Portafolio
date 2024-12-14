65ac486a1a9395196e9447975013d8f79d682db7
Ivan <ivangyr321@gmail.com>
2024-12-14 15:58:17 -0500

    Refactor Footer.svelte: actualiza el color del borde inferior en modo oscuro a sky-100

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<div class="rounded-3xl border-t-2 border-sky-900 dark:border-sky-300">
+<div class="rounded-3xl border-t-2 border-sky-900 dark:border-sky-100">
 	<footer class="container mx-auto px-4 py-6">
 		<p class="text-center text-slate-600 dark:text-slate-400">
 			© {new Date().getFullYear()} - Creado con
