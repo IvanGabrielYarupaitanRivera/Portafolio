94fc86ca73372e966be005602c2b233c3a647100
Ivan <ivangyr321@gmail.com>
2024-12-14 13:13:11 -0500

    Refactor +page.svelte: actualiza el contenido y estilo del encabezado y párrafo en la página 'Practicando'

--- src/routes/practicando/+page.svelte
+++ src/routes/practicando/+page.svelte
@@ -6,18 +6,12 @@
                 hover:translate-x-0.5 dark:border-sky-300
                  "
 	>
-		<h2
-			class="font-cabinet text-2xl font-bold
-                   text-slate-800 dark:text-slate-50"
-		>
-			Título 2
-		</h2>
+		<h2 class="heading-2">Título 2</h2>
 
-		<p
-			class=" 
-                  text-slate-600 dark:text-slate-400"
-		>
-			Contenido de ejemplo
+		<p class="p">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, quisquam nihil animi,
+			consequatur qui perferendis placeat ipsum dolores sequi quas rem mollitia debitis officia
+			architecto autem quo repellendus ducimus maiores?
 		</p>
 	</div>
 </div>
