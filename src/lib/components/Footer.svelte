f042c1f210803d3abece81b8df5341bbb2aa5f47
Ivan <ivangyr321@gmail.com>
2024-12-14 10:14:01 -0500

    Refactor Footer.svelte: actualiza colores del pie de página para mejorar la legibilidad y la estética

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,6 @@
-<div class="rounded-3xl border-t-2 border-blue-950 dark:border-orange-100">
+<div class="rounded-3xl border-t-2 border-sky-900 dark:border-sky-300">
 	<footer class="container mx-auto px-4 py-6">
-		<p class="text-center text-blue-800 dark:text-orange-100">
+		<p class="text-center text-slate-600 dark:text-slate-400">
 			© {new Date().getFullYear()} - Creado con
 			<span class="inline-block hover:-translate-y-1">💖</span>
 		</p>
