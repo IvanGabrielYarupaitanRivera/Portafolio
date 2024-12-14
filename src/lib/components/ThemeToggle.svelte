27f543411b11369c7ac3ba8767a0340b5d0cd8fe
Ivan <ivangyr321@gmail.com>
2024-12-14 10:14:08 -0500

    Refactor ThemeToggle.svelte: actualiza el color del borde del botón de cambio de tema para mejorar la visibilidad

--- src/lib/components/ThemeToggle.svelte
+++ src/lib/components/ThemeToggle.svelte
@@ -8,7 +8,7 @@
 
 <button
 	on:click={toggleTheme}
-	class="relative rounded-3xl border-2 border-sky-600 p-2 hover:-translate-y-0.5 hover:translate-x-0.5 active:translate-x-0 active:translate-y-0 dark:border-sky-400"
+	class="relative rounded-3xl border-2 border-sky-900 p-2 hover:-translate-y-0.5 hover:translate-x-0.5 active:translate-x-0 active:translate-y-0 dark:border-sky-300"
 	aria-label="Cambiar tema"
 >
 	<SunMoon
@@ -16,5 +16,5 @@
 		strokeWidth={2}
 		class="stroke-slate-800  
                dark:stroke-slate-50"
-	/>
+	></SunMoon>
 </button>
