cd3c0ff969d1829a1a242a7e8af88f2c1eab25fb
Ivan <ivangyr321@gmail.com>
2024-12-14 02:44:01 -0500

    Refactor Footer.svelte: actualiza estilos del pie de página con nuevos colores y bordes redondeados

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,6 @@
-<div class="border-t-2 border-black dark:border-white">
-	<footer class="container mx-auto px-4 py-6" aria-label="pie de página">
-		<p class="text-center text-zinc-700 dark:text-zinc-300">
+<div class="rounded-3xl border-t-2 border-blue-950 dark:border-orange-100">
+	<footer class="container mx-auto px-4 py-6">
+		<p class="text-center text-blue-800 dark:text-orange-100">
 			© {new Date().getFullYear()} - Creado con
 			<span class="inline-block hover:-translate-y-1">💖</span>
 		</p>
