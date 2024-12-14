e43bcc3f5753e7b2f77095dc8d888b3415f5b44c
Ivan <ivangyr321@gmail.com>
2024-12-13 22:56:01 -0500

    Agrega un botón para alternar entre modo claro y oscuro en ThemeToggle.svelte

---
+++ src/lib/components/ThemeToggle.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	function toggleTheme() {
+		document.documentElement.classList.toggle('dark');
+	}
+</script>
+
+<button onclick={toggleTheme} class="rounded-lg bg-gray-300 p-2 dark:bg-gray-700">🌓</button>
