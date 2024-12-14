9d0065ca5631edd3364ab4b99d7c749aa8a56728
Ivan <ivangyr321@gmail.com>
2024-12-14 10:14:15 -0500

    Refactor +page.svelte: actualiza colores de texto y bordes para mejorar la estética y la coherencia visual

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,26 @@
 <div class="space-y-8">
 	<h1
 		class="font-cabinet p-6
-	 text-6xl font-extrabold text-black dark:text-white"
+	 text-6xl font-extrabold text-slate-800 dark:text-slate-50"
 	>
 		Mi Portafolio
 	</h1>
 
 	<div
-		class="rounded-3xl border-2 border-black p-6
-                hover:-translate-y-0.5 hover:translate-x-0.5
-                dark:border-white"
+		class="rounded-3xl border-2 border-sky-900 p-6 hover:-translate-y-0.5
+                hover:translate-x-0.5 dark:border-sky-300
+                 "
 	>
 		<h3
 			class="font-cabinet text-2xl font-bold
-                   text-black dark:text-white"
+                   text-slate-800 dark:text-slate-50"
 		>
 			Título 3
 		</h3>
 
 		<p
 			class=" mt-2
-                  text-zinc-700 dark:text-zinc-300"
+                  text-slate-600 dark:text-slate-400"
 		>
 			Contenido de ejemplo
 		</p>
