96b9d41e415c0d36c4ba14ecb134102bfa45381d
Ivan <ivangyr321@gmail.com>
2024-12-14 15:25:07 -0500

    Refactor +page.svelte: elimina las transiciones de desplazamiento en el contenedor principal

--- src/routes/practicando/+page.svelte
+++ src/routes/practicando/+page.svelte
@@ -2,8 +2,7 @@
 	<h1 class="heading-1">Practicando</h1>
 
 	<div
-		class="rounded-3xl border-2 border-sky-900 p-6 hover:-translate-y-0.5
-                hover:translate-x-0.5 dark:border-sky-300
+		class="rounded-3xl border-2 border-sky-900 p-6 dark:border-sky-300
                  "
 	>
 		<h2 class="heading-2">Título 2</h2>
