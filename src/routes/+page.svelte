3d9e7f6fbb1e27b399a7031fb4e3e2b6fe1a2cd3
Ivan <ivangyr321@gmail.com>
2024-12-14 00:00:12 -0500

    Actualiza el estilo de la página principal: cambia el título a un encabezado h1 con fuente 'Cabinet Grotesk' y ajusta el contenido a un formato más destacado.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <div class="rounded-lg bg-gray-300 px-6 py-8 dark:bg-slate-800">
-	<h3 class="text-base font-bold text-slate-900 dark:text-white">Título</h3>
-	<p class="mt-2 text-sm text-slate-600 dark:text-slate-400">Contenido de ejemplo</p>
+	<h1 class="font-cabinet text-6xl font-extrabold text-slate-900 dark:text-white">hola</h1>
+	<h3 class="text-2xl font-bold text-slate-900 dark:text-white">Título 3</h3>
+	<p class="mt-2 font-medium text-slate-600 dark:text-slate-400">Contenido de ejemplo</p>
 </div>
