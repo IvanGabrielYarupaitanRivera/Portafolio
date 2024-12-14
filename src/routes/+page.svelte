5105ed15fddcb288a944beb3af828a34f2c65c1f
Ivan <ivangyr321@gmail.com>
2024-12-14 15:58:32 -0500

    Refactor +page.svelte: actualiza el color del borde en modo oscuro a sky-100

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 
 	<div
 		class="rounded-3xl border-2 border-sky-900 p-6 hover:-translate-y-0.5
-                hover:translate-x-0.5 dark:border-sky-300
+                hover:translate-x-0.5 dark:border-sky-100
                  "
 	>
 		<h2 class="heading-2">Portafolio</h2>
