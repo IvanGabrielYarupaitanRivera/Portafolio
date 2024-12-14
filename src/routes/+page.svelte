eca447d6f3a255e7d4c81ecfd6ee6a2d285eb1f7
Ivan <ivangyr321@gmail.com>
2024-12-14 17:56:36 -0500

    Refactor +page.svelte: reemplaza el contenido del componente con HeroSection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,5 @@
-<div class="space-y-8">
-	<h1 class="heading-1">Mi Portafolio</h1>
+<script lang="ts">
+	import HeroSection from './HeroSection.svelte';
+</script>
 
-	<div
-		class="rounded-3xl border-2 border-sky-900 p-6 hover:-translate-y-0.5
-                hover:translate-x-0.5 dark:border-sky-100
-                 "
-	>
-		<h2 class="heading-2">Portafolio</h2>
-
-		<p class="p">
-			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil assumenda explicabo at alias
-			delectus officiis, exercitationem commodi, aut corrupti quis sapiente expedita nulla libero
-			voluptatum atque odit odio dolorum. Laborum.
-		</p>
-	</div>
-</div>
+<HeroSection></HeroSection>
