9b357b127617d094d3a08157092607dd732e083f
Ivan <ivangyr321@gmail.com>
2024-12-14 10:14:04 -0500

    Refactor Header.svelte: actualiza colores del encabezado y mejora la accesibilidad de los enlaces

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,16 +2,16 @@
 	import ThemeToggle from './ThemeToggle.svelte';
 </script>
 
-<header class="rounded-3xl border-b-2 border-blue-950 dark:border-orange-100">
+<header class="rounded-3xl border-b-2 border-sky-900 dark:border-sky-300">
 	<nav class="container mx-auto px-4 py-6" aria-label="navegación principal">
 		<div class="flex items-center justify-between">
 			<a
 				href="/"
-				class="font-cabinet rounded-3xl border-2 border-blue-950 px-4
-					   py-2 text-xl font-bold text-blue-950
-					   hover:-translate-y-0.5 hover:translate-x-0.5
-					   active:translate-x-0 active:translate-y-0
-					   dark:border-orange-100 dark:text-orange-50"
+				class="font-cabinet rounded-3xl border-2 border-sky-900 px-4 py-2
+					   text-xl font-bold text-slate-800 hover:-translate-y-0.5
+					   hover:translate-x-0.5 active:translate-x-0
+					   active:translate-y-0 dark:border-sky-300
+					    dark:text-slate-50"
 			>
 				Ivan
 			</a>
