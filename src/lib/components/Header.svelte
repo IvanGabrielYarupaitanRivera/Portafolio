f4a9d0aeebebdc6c85bc2046f59d83d3dfa9aa5f
Ivan <ivangyr321@gmail.com>
2024-12-14 02:44:05 -0500

    Refactor Header.svelte: actualiza estilos del encabezado con bordes redondeados y nuevos colores

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,16 +2,16 @@
 	import ThemeToggle from './ThemeToggle.svelte';
 </script>
 
-<header class="border-b-2 border-black dark:border-white">
+<header class="rounded-3xl border-b-2 border-blue-950 dark:border-orange-100">
 	<nav class="container mx-auto px-4 py-6" aria-label="navegación principal">
 		<div class="flex items-center justify-between">
 			<a
 				href="/"
-				class="font-cabinet border-2 border-black px-4
-                       py-2 text-xl font-bold text-black
-                       hover:-translate-y-0.5 hover:translate-x-0.5
-                       active:translate-x-0 active:translate-y-0
-                       dark:border-white dark:text-white"
+				class="font-cabinet rounded-3xl border-2 border-blue-950 px-4
+					   py-2 text-xl font-bold text-blue-950
+					   hover:-translate-y-0.5 hover:translate-x-0.5
+					   active:translate-x-0 active:translate-y-0
+					   dark:border-orange-100 dark:text-orange-50"
 			>
 				Ivan
 			</a>
