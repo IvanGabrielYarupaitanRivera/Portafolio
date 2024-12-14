f8e0a27092e044ee86d17fc61c610f4cb4959674
Ivan <ivangyr321@gmail.com>
2024-12-14 00:01:46 -0500

    Elimina la definición de la fuente 'Cabinet Grotesk' en bold de app.css y actualiza el título en +page.svelte a 'Mi Portafolio' con un color de texto ajustado.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div class="rounded-lg bg-gray-300 px-6 py-8 dark:bg-slate-800">
-	<h1 class="font-cabinet text-6xl font-extrabold text-slate-900 dark:text-white">hola</h1>
-	<h3 class="text-2xl font-bold text-slate-900 dark:text-white">Título 3</h3>
+	<h1 class="font-cabinet text-6xl font-extrabold text-slate-950 dark:text-white">Mi Portafolio</h1>
+	<h3 class="text-2xl font-bold text-slate-900 dark:text-slate-300">Título 3</h3>
 	<p class="mt-2 font-medium text-slate-600 dark:text-slate-400">Contenido de ejemplo</p>
 </div>
