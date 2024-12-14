f2c9e39ba1b3305740cf0f5728e28b8b27219fbc
Ivan <ivangyr321@gmail.com>
2024-12-13 22:56:10 -0500

    Reemplaza el contenido de la página principal con un nuevo diseño y estilo para mejorar la presentación

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="rounded-lg bg-gray-300 px-6 py-8 dark:bg-slate-800">
+	<h3 class="text-base font-medium text-slate-900 dark:text-white">Título</h3>
+	<p class="mt-2 text-sm text-slate-600 dark:text-slate-400">Contenido de ejemplo</p>
+</div>
