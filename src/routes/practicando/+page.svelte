bbea8f9c5c7af345e50fd3cad1288ff01d90a3f0
Ivan <ivangyr321@gmail.com>
2024-12-14 12:39:03 -0500

    Refactor +page.svelte: añade nueva página 'Practicando' con encabezado y contenido de ejemplo

---
+++ src/routes/practicando/+page.svelte
@@ -0,0 +1,23 @@
+<div class="space-y-8">
+	<h1 class="heading-1">Practicando</h1>
+
+	<div
+		class="rounded-3xl border-2 border-sky-900 p-6 hover:-translate-y-0.5
+                hover:translate-x-0.5 dark:border-sky-300
+                 "
+	>
+		<h2
+			class="font-cabinet text-2xl font-bold
+                   text-slate-800 dark:text-slate-50"
+		>
+			Título 2
+		</h2>
+
+		<p
+			class=" 
+                  text-slate-600 dark:text-slate-400"
+		>
+			Contenido de ejemplo
+		</p>
+	</div>
+</div>
