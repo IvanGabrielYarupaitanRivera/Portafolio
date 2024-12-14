37cbb9380100fc57c7499b7c8cca67c64cddf78c
Ivan <ivangyr321@gmail.com>
2024-12-14 15:33:27 -0500

    Refactor Header.svelte: actualiza el color de fondo del enlace a naranja 400 y mejora el estilo de hover

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,11 +7,11 @@
 		<div class="flex justify-between">
 			<a
 				href="/"
-				class="font-cabinet rounded-3xl border-2 border-sky-900 bg-orange-500 px-4 py-2
+				class="font-cabinet rounded-3xl border-2 border-sky-900 bg-orange-400 px-4 py-2
 					   text-xl font-bold text-slate-800 hover:-translate-y-0.5
-					   hover:translate-x-0.5 hover:bg-orange-400 active:translate-x-0 active:translate-y-0
-					   dark:border-sky-300 dark:bg-orange-500 dark:text-slate-50
-					    dark:hover:bg-orange-600"
+					   hover:translate-x-0.5 hover:bg-orange-300 active:translate-x-0 active:translate-y-0
+					   dark:border-sky-300 dark:bg-orange-600 dark:text-slate-50
+					    dark:hover:bg-orange-700"
 			>
 				Ivan Gabriel
 			</a>
