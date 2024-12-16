641d1faa6400168886a5da5719e181c3ac69ee2c
Ivan <ivangyr321@gmail.com>
2024-12-16 04:39:35 -0500

    Actualizar estilos en el componente HeroSection: simplificar clases y mejorar la legibilidad

--- src/routes/HeroSection.svelte
+++ src/routes/HeroSection.svelte
@@ -1,31 +1,17 @@
 <section class="relative z-10">
 	<!-- Texto principal -->
 	<div class="flex flex-col items-center gap-6 text-center">
-		<h1 class="text-pretty font-cabinet text-5xl font-extrabold text-slate-50 md:text-6xl">
-			Hola, soy <span class="text-sky-400">Ivan Gabriel</span>
+		<h1 class="heading-1">
+			Hola, soy <span class="text-sky-800 dark:text-sky-400">Ivan Gabriel</span>
 		</h1>
 
-		<p class="text-pretty text-base leading-relaxed text-slate-400 md:text-lg">
-			Desarrollador Full Stack apasionado por crear experiencias web excepcionales
-		</p>
+		<p class="p">Desarrollador Full Stack apasionado por crear experiencias web excepcionales</p>
 
 		<div class="flex flex-wrap justify-center gap-4">
-			<a
-				href="#proyectos"
-				class="rounded-3xl border-2 border-sky-100 bg-orange-600 px-6 py-3
-                    font-bold text-slate-50 hover:-translate-y-0.5 hover:translate-x-0.5
-                    hover:bg-orange-700 active:translate-x-0 active:translate-y-0
-                    "
-			>
+			<a href="#proyectos" class="my-effect my-border my-bg p border-2 px-6 py-3 font-bold">
 				Ver Proyectos
 			</a>
-			<a
-				href="#contacto"
-				class="rounded-3xl border-2 border-sky-100 px-6 py-3
-                    font-bold text-slate-50 hover:-translate-y-0.5 hover:translate-x-0.5
-                    active:translate-x-0 active:translate-y-0
-                     "
-			>
+			<a href="#contacto" class=" my-border p my-effect border-2 px-6 py-3 font-bold">
 				Contáctame
 			</a>
 		</div>
