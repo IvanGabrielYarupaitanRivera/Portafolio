d3ea64f3dde8126f1f19f6a70c97b5cc436eb684
Ivan <ivangyr321@gmail.com>
2024-12-16 04:39:28 -0500

    Actualizar estilos en el componente Technologies: simplificar clases y mejorar la estructura

--- src/routes/Technologies.svelte
+++ src/routes/Technologies.svelte
@@ -34,13 +34,7 @@
 	<div class="mt-8 flex flex-row flex-wrap justify-center gap-6 md:gap-8">
 		{#each technologies as tech}
 			<div
-				class="flex w-32 flex-col items-center gap-2 rounded-3xl border-2 border-sky-900 bg-white px-2 pb-4 pt-6
-				 
-				hover:-translate-y-1
-                hover:translate-x-1 active:translate-x-0 active:translate-y-0 md:w-44
-                md:gap-4 md:px-4 md:pb-6 md:pt-8 dark:border-sky-100
-                 dark:bg-neutral-800
-                "
+				class="my-effect my-border my-component-bg flex w-32 flex-col items-center gap-2 border-2 px-2 pb-4 pt-6 md:w-44 md:gap-4 md:px-4 md:pb-6 md:pt-8"
 			>
 				<div class="  h-16 w-16 md:h-24 md:w-24">
 					<img src={tech.logo} alt={tech.name} />
