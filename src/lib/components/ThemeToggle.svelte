5884569a568a0dcdb90767091d0d3d8d817baa73
Ivan <ivangyr321@gmail.com>
2024-12-16 04:39:17 -0500

    Actualizar estilos en el componente ThemeToggle: simplificar clases y mejorar la accesibilidad

--- src/lib/components/ThemeToggle.svelte
+++ src/lib/components/ThemeToggle.svelte
@@ -8,13 +8,8 @@
 
 <button
 	onclick={toggleTheme}
-	class=" rounded-3xl border-2 border-sky-900 bg-orange-400 p-2 hover:-translate-y-0.5 hover:translate-x-0.5 hover:bg-orange-300 active:translate-x-0 active:translate-y-0 dark:border-sky-100 dark:bg-orange-600 dark:hover:bg-orange-700"
+	class="my-border my-bg my-effect border-2 p-2"
 	aria-label="Cambiar tema"
 >
-	<SunMoon
-		size={24}
-		strokeWidth={2}
-		class="stroke-slate-800  
-               dark:stroke-slate-50"
-	></SunMoon>
+	<SunMoon size={24} strokeWidth={2} class="my-stroke"></SunMoon>
 </button>
