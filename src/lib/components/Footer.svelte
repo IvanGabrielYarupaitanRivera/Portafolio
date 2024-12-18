ef926831d0965bca1b75faa8c8ae583f90e29263
Ivan <ivangyr321@gmail.com>
2024-12-18 03:37:33 -0500

    Actualizar Footer.svelte: agregar iconos de redes sociales y mejorar la estructura del pie de página

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,8 +1,20 @@
-<div class="my-component-bg my-border border-t-2">
-	<footer class="container mx-auto px-4 py-6">
+<script>
+	import { Github, LinkedinIcon } from 'lucide-svelte';
+</script>
+
+<footer class="my-component-bg my-border container border-t-2 px-4 py-6">
+	<div class="mx-auto flex flex-col items-center gap-4">
+		<div class="flex gap-6">
+			<a href="/">
+				<Github class="my-stroke" size={20} />
+			</a>
+			<a href="/">
+				<LinkedinIcon class="my-stroke" size={20} />
+			</a>
+		</div>
 		<p class="p text-center">
 			© {new Date().getFullYear()} - Creado con
 			<span class="inline-block hover:-translate-y-1">💖</span>
 		</p>
-	</footer>
-</div>
+	</div>
+</footer>
