aabf5c2865af02611b8e03eaa93d553a22285d62
Ivan <ivangyr321@gmail.com>
2024-12-19 14:28:39 -0500

    Eliminar clase innecesaria en el componente Header.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,7 +2,7 @@
 	import NavMenu from './NavMenu.svelte';
 </script>
 
-<header class="my-border my-component-bg relative z-20 border-b-2">
+<header class="my-border my-component-bg z-20 border-b-2">
 	<nav class="container mx-auto px-4 py-6" aria-label="navegación principal">
 		<div class="flex justify-between">
 			<a href="/" class="my-border my-bg heading-3 my-effect my-shadow border-2 px-4 py-2">
