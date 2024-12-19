376602fbdcb9fdff177359517adc44426ccf43e5
Ivan <ivangyr321@gmail.com>
2024-12-18 20:53:59 -0500

    Actualizar Header.svelte: agregar sombra al enlace del encabezado

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,7 +5,9 @@
 <header class="my-border my-component-bg relative z-20 border-b-2">
 	<nav class="container mx-auto px-4 py-6" aria-label="navegación principal">
 		<div class="flex justify-between">
-			<a href="/" class="my-border my-bg heading-3 my-effect border-2 px-4 py-2"> Ivan Gabriel </a>
+			<a href="/" class="my-border my-bg heading-3 my-effect my-shadow border-2 px-4 py-2">
+				Ivan Gabriel
+			</a>
 
 			<NavMenu />
 		</div>
