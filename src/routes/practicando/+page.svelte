80957da4df5ef81375c8977df2f92c6d884934fb
Ivan <ivangyr321@gmail.com>
2024-12-21 17:20:17 -0500

    Duplicar encabezados en +page.svelte para pruebas de diseño

--- src/routes/practicando/+page.svelte
+++ src/routes/practicando/+page.svelte
@@ -2,3 +2,28 @@
 </script>
 
 <h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
+<h1 class="heading-1">Hola</h1>
