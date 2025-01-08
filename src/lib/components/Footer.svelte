feb22e4100d3c84b4d7b3fdd23004df0b292f7e7
Ivan <ivangyr321@gmail.com>
2025-01-08 06:36:15 -0500

    Actualizar enlaces en el pie de página para incluir perfiles de GitHub y LinkedIn

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,11 +5,24 @@
 <footer class="my-component-bg my-border border-t-2 px-4 py-6">
 	<div class="mx-auto flex flex-col items-center gap-4">
 		<div class="flex gap-6">
-			<a href="/">
+			<a
+				href="https://github.com/IvanGabrielYarupaitanRivera"
+				target="_blank"
+				rel="noopener noreferrer"
+				aria-label="Visita mi perfil de GitHub"
+			>
 				<Github class="my-stroke" size={24} />
+				<span class="sr-only">GitHub</span>
 			</a>
-			<a href="/">
+
+			<a
+				href="https://www.linkedin.com/in/ivan-yarupaitan-rivera/"
+				target="_blank"
+				rel="noopener noreferrer"
+				aria-label="Visita mi perfil de LinkedIn"
+			>
 				<LinkedinIcon class="my-stroke" size={24} />
+				<span class="sr-only">LinkedIn</span>
 			</a>
 		</div>
 		<p class="p text-center">
