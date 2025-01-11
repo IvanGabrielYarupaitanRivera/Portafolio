8129cd82fda58e8b26bb9028d86db698ba3b3087
Vanchi <ivangyr321@gmail.com>
2025-01-10 22:27:49 -0500

    Ajustar el padding derecho del menú de navegación en el encabezado

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header class="my-border my-component-bg z-20 border-b-2">
-	<nav class="container mx-auto pb-6 pl-4 pr-6 pt-4" aria-label="navegación principal">
+	<nav class="container mx-auto pb-6 pl-4 pr-8 pt-4" aria-label="navegación principal">
 		<div class="flex justify-between">
 			<a href="/" class="my-border my-bg heading-3 my-effect my-shadow border-2 px-4 py-2">
 				Ivan Gabriel
