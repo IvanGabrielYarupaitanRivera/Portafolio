2433850967f7fdb72ecbd9154ea95b8caf879848
Ivan <ivangyr321@gmail.com>
2025-02-18 00:24:07 -0500

    Actualizar menú de navegación móvil: agregar nuevos elementos y mejorar estilos

--- src/lib/components/NavMenu.svelte
+++ src/lib/components/NavMenu.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
 	import { Menu, X } from 'lucide-svelte';
 	import { page } from '$app/stores';
-	import { slide } from 'svelte/transition';
+
+	let { mobileMenu } = $props();
 
 	let isMenuOpen = $state(false);
 	let isCloseMenuByNav = $state(false);
 
-	const menuItems = [{ href: '/', text: 'Inicio' }];
+	const menuItems = [
+		{ href: '#proyectos', text: 'Proyectos' },
+		{ href: '#contacto', text: 'Contacto' }
+	];
 
 	const toggleMenu = () => (isMenuOpen = !isMenuOpen);
 
 	const isCurrentPage = $derived((href: string) => $page.url.pathname === href);
 </script>
 
-<header>
+<header class="  ">
 	<!-- Desktop Menu -->
 	<div class="hidden h-full items-center gap-6 md:flex">
 		<ul class="flex gap-6">
@@ -21,19 +25,9 @@
 				<li>
 					<a
 						href={item.href}
-						aria-current={isCurrentPage(item.href) ? 'page' : undefined}
-						class="relative px-1 py-2 font-bold
-                            {isCurrentPage(item.href)
-							? 'text-green-800 dark:text-green-400'
-							: 'text-neutral-500 dark:text-neutral-200'}"
+						class="relative px-1 py-2 font-bold text-neutral-500 dark:text-neutral-200"
 					>
 						{item.text}
-						{#if isCurrentPage(item.href)}
-							<div
-								class="absolute bottom-0 left-0 h-[4px] w-full bg-green-800"
-								style:view-transition-name="nav-indicator"
-							></div>
-						{/if}
 					</a>
 				</li>
 			{/each}
@@ -41,16 +35,13 @@
 	</div>
 
 	<!-- Mobile Controls -->
-	<div class="flex items-center gap-4 md:hidden">
+	<div class="flex items-center md:hidden">
 		<button
 			type="button"
-			class="my-border my-effect my-shadow my-bg border-2 p-2"
+			class="my-border my-effect my-shadow my-bg rounded-xl border-2 p-2"
 			aria-expanded={isMenuOpen}
 			aria-label={isMenuOpen ? 'Cerrar menú principal' : 'Abrir menú principal'}
-			onclick={() => {
-				toggleMenu();
-				isCloseMenuByNav = true;
-			}}
+			onclick={mobileMenu}
 		>
 			{#if isMenuOpen}
 				<X size={30} strokeWidth={2} class="my-stroke" />
@@ -59,43 +50,4 @@
 			{/if}
 		</button>
 	</div>
-
-	<!-- Mobile Menu -->
-	{#if isMenuOpen}
-		<nav
-			id="mobile-menu"
-			aria-label="Menú móvil"
-			class="my-border my-component-bg absolute left-0 right-0 top-24 z-10 -mt-4 border-b-2 pb-4 pt-4 md:hidden"
-			in:slide={{ duration: 300 }}
-			out:slide={{ duration: isCloseMenuByNav ? 300 : 0 }}
-		>
-			<ul class="container mx-auto flex flex-col gap-4 px-4 pb-4 pt-1">
-				{#each menuItems as item}
-					<li>
-						<a
-							onclick={() => {
-								toggleMenu();
-								isCloseMenuByNav = false;
-							}}
-							href={item.href}
-							aria-current={isCurrentPage(item.href) ? 'page' : undefined}
-							class="relative px-1 py-2 font-bold
-                            {isCurrentPage(item.href)
-								? 'text-green-800 underline decoration-4 underline-offset-8 dark:text-green-400'
-								: 'text-neutral-500 dark:text-neutral-200'}"
-						>
-							{item.text}
-						</a>
-					</li>
-				{/each}
-			</ul>
-		</nav>
-	{/if}
 </header>
-
-<style>
-	::view-transition-old(nav-indicator),
-	::view-transition-new(nav-indicator) {
-		height: 4px;
-	}
-</style>
