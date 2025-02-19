ade053e96e6b1a1d331320d8708e258b728c806e
Ivan <ivangyr321@gmail.com>
2025-02-19 15:09:59 -0500

    Mejorar estilos en la página de error: ajustar z-index, espaciado y agregar cursor pointer al botón

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,15 +2,15 @@
 	import { page } from '$app/stores';
 </script>
 
-<section class="flex grow items-center justify-center">
-	<div class="  container mx-auto max-w-2xl rounded-xl p-8 text-center">
-		<h1 class="heading-1 mb-2">Error {$page.status}</h1>
+<section class="z-10 flex grow items-center justify-center">
+	<div class="container mx-auto max-w-2xl text-center">
+		<h1 class="heading-1 mb-1">Error {$page.status}</h1>
 		<p class="mb-8 text-lg text-neutral-600">
 			{$page.error?.message || 'Página no encontrada'}
 		</p>
 		<a
 			href="/"
-			class="my-border my-bg my-effect my-shadow inline-block rounded-xl border-2 px-6 py-3 font-bold text-neutral-950"
+			class="my-border my-bg my-effect my-shadow inline-block cursor-pointer rounded-xl border-2 px-6 py-3 font-bold text-neutral-950"
 		>
 			Volver al inicio
 		</a>
