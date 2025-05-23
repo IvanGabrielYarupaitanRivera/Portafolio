275f5004b11f43a7a24355e0d26d16765c9d24bf
Ivan <ivangyr321@gmail.com>
2025-05-23 16:05:44 -0500

    Agregar nueva página de proyectos con secciones de héroe y lista de proyectos

---
+++ src/routes/proyectos/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Header from '$lib/components/MainPage/Header/Header.svelte';
+	import HeroSection from './components/HeroSection.svelte';
+	import Projects from './components/Projects.svelte';
+</script>
+
+<Header />
+
+<main class="z-10 container mx-auto flex flex-1 py-6">
+	<article class="relative mr-4 ml-3 flex flex-col gap-48">
+		<section class="mt-16 md:px-36" aria-label="Proyectos destacados">
+			<HeroSection />
+		</section>
+
+		<section aria-label="Lista de proyectos">
+			<Projects />
+		</section>
+	</article>
+</main>
