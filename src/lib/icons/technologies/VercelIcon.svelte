aadcd044b21e750cf4dd728f40d27d86a79c122a
Ivan <ivangyr321@gmail.com>
2025-05-25 22:08:18 -0500

    Agregar nuevo componente VercelIcon en Svelte

---
+++ src/lib/icons/technologies/VercelIcon.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { size = 24, color = 'currentColor', class: className = '' } = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width={size}
+	height={size}
+	viewBox="0 0 24 24"
+	class={className}
+>
+	<path fill={color} d="M23 21.648H1L12 2.352z" />
+</svg>
