fe6ace7c4e3b26ad4b41791466fd4e704b171fed
Ivan <ivangyr321@gmail.com>
2025-05-25 19:05:46 -0500

    Agregar nuevo ícono de Supabase con soporte para tamaño, color y clases personalizadas

---
+++ src/lib/icons/technologies/SupabaseIcon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let { size = 24, color = 'currentColor', class: className = '' } = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width={size}
+	height={size}
+	viewBox="0 0 24 24"
+	class={className}
+>
+	<path
+		fill={color}
+		d="M3.084 15.25c-1.664 0-2.6-1.912-1.58-3.226L10.21.806C10.794.054 12 .466 12 1.42v7.33h8.916c1.663 0 2.6 1.912 1.58 3.226L13.79 23.194c-.584.752-1.79.34-1.79-.613V15.25z"
+	/>
+</svg>
